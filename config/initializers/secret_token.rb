5d2e8b0cc8bc4161da3f979fd10af9a2f410e1f4
Francisco J. Aneiros Ramírez <Nightcrawler@MacBook-Pro-de-Francisco.local>
2013-12-14 11:28:32 +0000

    first project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PokerProject::Application.config.secret_key_base = '31d2e521d421c61d1ffb84441473d1ff16d39012cc428d899a50312814ac1d5e6e24660861ac424ce0ae75ce0aea149ea155b26b3f322ea9409119badf6cf449'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PokerProject::Application.config.session_store :cookie_store, key: '_poker_project_session'
